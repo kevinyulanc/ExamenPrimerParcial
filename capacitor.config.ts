a6b7ea4549a7306176e1539f76fc8057b07228dd
Kevin Yulan <kevinyulan99@gmail.com>
2024-11-28 18:07:59 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'password-validation',
+  webDir: 'www'
+};
+
+export default config;
