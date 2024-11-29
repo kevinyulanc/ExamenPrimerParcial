9bd6ce2110d7d02a2b3228a8ec654c1a872a2315
Kevin Yulan <kevinyulan99@gmail.com>
2024-11-28 20:40:20 -0500

    Nuevo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  usuario: string = '';  // Definir la variable usuario
+  correo: string = '';  // Definir la variable correo
+  password: string = '';  // Definir la variable password
+  confirmarPassword: string = '';  // Definir la variable confirmarPassword
 
-  constructor() {}
+  constructor(private alertController: AlertController) {}
 
+  async validarContrasenas() {
+    if (this.password === this.confirmarPassword) {
+      // Si las contraseñas coinciden
+      const alert = await this.alertController.create({
+        header: 'Éxito',
+        message: '¡Bienvenido!',
+        buttons: ['OK']
+      });
+      await alert.present();
+    } else {
+      // Si las contraseñas no coinciden
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'Las contraseñas no coinciden. Por favor, intente de nuevo.',
+        buttons: ['Intentar de nuevo']
+      });
+      await alert.present();
+    }
+  }
 }
